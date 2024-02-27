88b05a197ba3cd3e076fa1cfebdce84a2b9c2def
Ghie Cotcheza <ghiecotcheza@msml.nl>
2024-02-27 17:31:52 +0100

    Add user gate and links to navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,14 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @can('schedule-class')
+                    <x-nav-link :href="route('schedule.create')" :active="request()->routeIs('schedule.create')">
+                        Schedule a class
+                    </x-nav-link>
+                    <x-nav-link :href="route('schedule.index')" :active="request()->routeIs('schedule.index')">
+                        Upcoming classes
+                    </x-nav-link>
+                    @endcan
                 </div>
             </div>
 
@@ -69,6 +77,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('schedule.create')" :active="request()->routeIs('schedule.create')">
+                Schedule a class
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('schedule.index')" :active="request()->routeIs('schedule.index')">
+                Upcoming classes
             </x-responsive-nav-link>
         </div>
 
