4e44f92c661693a9b4eccca4f85f2bf16d063de7
Ghie Cotcheza <ghiecotcheza@msml.nl>
2024-03-19 09:40:17 +0100

    code clean up

--- database/factories/ScheduledClassFactory.php
+++ database/factories/ScheduledClassFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -12,8 +11,6 @@ class ScheduledClassFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
-     * @return array<string, mixed>
      */
     public function definition(): array
     {
